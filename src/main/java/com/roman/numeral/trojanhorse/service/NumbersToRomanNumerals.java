8c618ad75b1a3dbb26bac2731f6fa69c907e534a
Jamal Khaffaf <jamaljamal@Jamals-MacBook-Pro.local>
2023-11-02 09:12:04 -0400

    Added negative test case for converting less than one.

--- src/main/java/com/roman/numeral/trojanhorse/service/NumbersToRomanNumerals.java
+++ src/main/java/com/roman/numeral/trojanhorse/service/NumbersToRomanNumerals.java
@@ -1,5 +1,10 @@
 package com.roman.numeral.trojanhorse.service;
 
 public class NumbersToRomanNumerals {
+
+    public String covert(int number) {
+
+        return "";
+    }
     
 }
--- src/test/java/com/roman/numeral/trojanhorse/service/NumbersToRomanNumeralsTest.java
+++ src/test/java/com/roman/numeral/trojanhorse/service/NumbersToRomanNumeralsTest.java
@@ -1,7 +1,19 @@
 package com.roman.numeral.trojanhorse.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class NumbersToRomanNumeralsTest {
+
+    NumbersToRomanNumerals suit = new NumbersToRomanNumerals();
+
+    @Test
+    public void lessThanOneTest() {
+        IllegalArgumentException ex = assertThrowsExactly(IllegalArgumentException.class, () -> suit.covert(0));
+        assertEquals("Only Numbers Greater Than 0 And Less Than 3000 Are Accepted!", ex.getMessage());
+    }
 }
