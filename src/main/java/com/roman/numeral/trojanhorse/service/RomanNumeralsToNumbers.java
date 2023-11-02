a44e0a86851cde1dba57dbf0d731e0620b054733
Jamal Khaffaf <jamaljamal@Jamals-MacBook-Pro.local>
2023-11-02 11:09:27 -0400

    Add test coverage for null and empty values, in roman numerals to numbers. In addition, added convert method skeleton.

--- src/main/java/com/roman/numeral/trojanhorse/service/RomanNumeralsToNumbers.java
+++ src/main/java/com/roman/numeral/trojanhorse/service/RomanNumeralsToNumbers.java
@@ -1,5 +1,9 @@
 package com.roman.numeral.trojanhorse.service;
 
 public class RomanNumeralsToNumbers {
-    
+
+    public int convert(String romanNumeral) {
+        
+        return -1;
+    }
 }
--- src/test/java/com/roman/numeral/trojanhorse/service/RomanNumeralsToNumbersTest.java
+++ src/test/java/com/roman/numeral/trojanhorse/service/RomanNumeralsToNumbersTest.java
@@ -1,7 +1,25 @@
 package com.roman.numeral.trojanhorse.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class RomanNumeralsToNumbersTest {
+
+    RomanNumeralsToNumbers suit = new RomanNumeralsToNumbers();
+
+    @Test
+    public void sendNullObjectExpectIllegalArgumentException() {
+        IllegalArgumentException ex = assertThrowsExactly(IllegalArgumentException.class, () -> suit.convert(null));
+        assertEquals("Invalid argument been used, Null or Empty are not valid!", ex.getMessage());
+    }
+
+    @Test
+    public void sendEmptyObjectExpectIllegalArgumentException() {
+        IllegalArgumentException ex = assertThrowsExactly(IllegalArgumentException.class, () -> suit.convert(""));
+        assertEquals("Invalid argument been used, Null or Empty are not valid!", ex.getMessage());
+    }
 }
