683ae778733fcc980a86acdf030e4754aef109e0
Jamal Khaffaf <jamaljamal@Jamals-MacBook-Pro.local>
2023-11-02 11:39:38 -0400

    Added code coverage for handling invalid roman numeral characters. Also added toUpperCase method to convert values.

--- src/main/java/com/roman/numeral/trojanhorse/service/RomanNumeralsToNumbers.java
+++ src/main/java/com/roman/numeral/trojanhorse/service/RomanNumeralsToNumbers.java
@@ -7,6 +7,11 @@ public class RomanNumeralsToNumbers {
         if (romanNumeral == null || romanNumeral.isEmpty()) {
             throw new IllegalArgumentException("Invalid argument been used, Null or Empty are not valid!");
         }
+
+        romanNumeral = romanNumeral.toUpperCase();
+        if (!romanNumeral.matches("[MDCLXVI]+")) {
+            throw new IllegalArgumentException("Invalid argument been detected, please use values within expected range [M, D, C, L, X, V, I].");
+        }
         
         return -1;
     }
