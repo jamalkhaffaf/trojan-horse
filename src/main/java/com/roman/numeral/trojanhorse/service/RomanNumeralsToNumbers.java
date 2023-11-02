ebcad6e0915388c72e7099394c27c761495ee2de
Jamal Khaffaf <jamaljamal@Jamals-MacBook-Pro.local>
2023-11-02 11:12:13 -0400

    Added code coverage for handling null and empty roman numeral values.

--- src/main/java/com/roman/numeral/trojanhorse/service/RomanNumeralsToNumbers.java
+++ src/main/java/com/roman/numeral/trojanhorse/service/RomanNumeralsToNumbers.java
@@ -3,6 +3,10 @@ package com.roman.numeral.trojanhorse.service;
 public class RomanNumeralsToNumbers {
 
     public int convert(String romanNumeral) {
+
+        if (romanNumeral == null || romanNumeral.isEmpty()) {
+            throw new IllegalArgumentException("Invalid argument been used, Null or Empty are not valid!");
+        }
         
         return -1;
     }
