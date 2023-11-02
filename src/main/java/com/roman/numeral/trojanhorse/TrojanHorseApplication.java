cfe527a075f9a0216ab776c5c8ae42c78e22c2ec
Jamal Khaffaf <jamaljamal@Jamals-MacBook-Pro.local>
2023-11-02 08:31:40 -0400

    Roman Numerals and Numbers Conversion Project

---
+++ src/main/java/com/roman/numeral/trojanhorse/TrojanHorseApplication.java
@@ -0,0 +1,13 @@
+package com.roman.numeral.trojanhorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrojanHorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrojanHorseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roman/numeral/trojanhorse/TrojanHorseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roman.numeral.trojanhorse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrojanHorseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
